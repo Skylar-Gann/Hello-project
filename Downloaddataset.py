81abc459423a0262b480e3e23884d5ffefc1fdc7
Skylar-Gann <skylar.gann@gmail.com>
2025-09-04 18:35:12 -0700

    Create Downloaddataset.py

---
+++ Downloaddataset.py
@@ -0,0 +1,6 @@
+import kagglehub
+
+# Download latest version of the dataset
+path = kagglehub.dataset_download("mdsultanulislamovi/student-stress-monitoring-datasets")
+
+print("Path to dataset files:", path)
